d8ca43ef74a797cf24fe85798ef9d3a5b85cace5
Aleksis <daugatsa@gmail.com>
2023-04-03 14:33:11 +0300

    test

--- daugats_ApskatiLatviju-master/ApskatiLatviju/login.js
+++ daugats_ApskatiLatviju-master/ApskatiLatviju/login.js
@@ -4,12 +4,15 @@ const contentDiv = document.getElementById("login-popup");
 toggleButton.addEventListener("click", () => {
   if (contentDiv.style.display === "none") {
     contentDiv.style.display = "block";
+    toggleButton.style.display = "none";
+  } else {
+    contentDiv.style.display = "none";
   }
 });
 
-document.addEventListener("click", (event) => {
-  if (!contentDiv.contains(event.target) && event.target !== toggleButton) {
-    contentDiv.style.display = "none";
-    
+document.addEventListener('mouseup', function(e) {
+  if (!contentDiv.contains(e.target)) {
+      contentDiv.style.display = 'none';
+      toggleButton.style.display = "block";
   }
 });
