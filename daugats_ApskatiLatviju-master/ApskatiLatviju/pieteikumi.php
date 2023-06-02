f019aa43680ffdb257ff0cf3d21ba66ee46f25ec
Aleksis Daugats <daugatsa@gmail.com>
2023-06-02 14:10:43 +0300

    Work

--- daugats_ApskatiLatviju-master/ApskatiLatviju/pieteikumi.php
+++ daugats_ApskatiLatviju-master/ApskatiLatviju/pieteikumi.php
@@ -1,35 +1,12 @@
 <?php
     include "header.php";
+    if (!isset($_SESSION["Lietotajvards"])) {
+        header("Refresh: 0; url=index.php");
+    }
 ?>
 
     <section id="sakums">
             <h1>Pieteikumi</h1>
-            <div class="slider-container">
-                <div class="slider">
-                  <img src="Images/Liepaja.jpg" alt="image1">
-                  <img src="Images/jelgava.jpg" alt="image2">
-                  <img src="Images/TopIMG.jpg" alt="image3">
-                </div>
-                <div class="navigation">
-                  <button class="prev">&#60;</button>
-                  <button class="next">&#62;</button>
-                </div>
-              </div>
-              <h1>Jaunākās aktualitates</h1>
-              <div id="jaunAkt">
-                <div class="JA-post">
-                    <img src="Images/Liepaja.jpg" alt="Post Image">
-                    <h2>Aktualitāte 1</h2>
-                </div>
-                <div class="JA-post">
-                    <img src="Images/Liepaja.jpg" alt="Post Image">
-                    <h2>Aktualitāte 2</h2>
-                </div>
-                <div class="JA-post">
-                    <img src="Images/Liepaja.jpg" alt="Post Image">
-                    <h2>Aktualitāte 3</h2>
-                </div>
-                </div>
     </section>
 
 
