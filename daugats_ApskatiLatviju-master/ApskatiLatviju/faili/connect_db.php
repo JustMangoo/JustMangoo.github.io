0235fe233110d34377af187898482c00f13a85ec
Aleksis Daugats <98739151+JustMangoo@users.noreply.github.com>
2023-11-22 10:04:25 +0200

    Delete daugats_ApskatiLatviju-master/ApskatiLatviju directory

--- daugats_ApskatiLatviju-master/ApskatiLatviju/faili/connect_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $serveravards = "localhost:3307";
-    $lietotajvards = "root";
-    $parole = "";
-    $dbvards = "apskatilatv";
-
-    $savienojums = mysqli_connect($serveravards, $lietotajvards, $parole, $dbvards);
-?>
